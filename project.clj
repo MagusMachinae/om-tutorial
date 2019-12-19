600cd786d7401258ce98bc92a16d895ea7a6b4a5
MagusMachinae <Edward.Hughes1911@gmail.com>
2019-12-19 18:36:17 +0000

    Dependencies ready

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.597"]
                  [org.omcljs/om "1.0.0-beta4"]
-                 []])
+                 [figwheel-sidecar "0.5.19"]])
